02cf56939df9f2c08921c25c6660ac730d86f996
wiwi <william.lecorre27@gmail.com>
2020-03-31 22:11:21 +0200

    CHECK Kauch

--- Linky2/app/src/main/java/fr/willy/linky/Graph.java
+++ Linky2/app/src/main/java/fr/willy/linky/Graph.java
@@ -34,7 +34,7 @@ public class Graph extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
 
-        //CHECK WILLY
+        //CHECK Kauch
 
 
         setContentView(R.layout.activity_graph);
