49601efb3b0f1581de7eec7547e77130569ff173
david <david.lesaout.1998@gmail.com>
2020-04-01 16:17:29 +0200

    Premier commit de David

--- Linky2/app/src/main/java/fr/willy/linky/MainActivity.java
+++ Linky2/app/src/main/java/fr/willy/linky/MainActivity.java
@@ -97,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
         // -----------------------------------------------------------------------------------------
         get_my_ip_address();
 
-        //WILLY 2
+        //testDavid
 
         //EBAUCHE BASE DE DONNE
 
