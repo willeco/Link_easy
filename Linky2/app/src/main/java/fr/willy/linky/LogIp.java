2a413faac93f04fd05b8c7ce6f54ebab92606658
ThomasCochou <cochouthomas@gmail.com>
2020-05-19 15:05:21 +0200

    creation Log IP

---
+++ Linky2/app/src/main/java/fr/willy/linky/LogIp.java
@@ -0,0 +1,14 @@
+package fr.willy.linky;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogIp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_ip);
+    }
+}
